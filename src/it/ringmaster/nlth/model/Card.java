aba1c71cdfa5800565f809d30934a9fda14efe3c
Reiner409 <72210285+Reiner409@users.noreply.github.com>
2022-04-12 10:43:57 +0200

    The base classes are created.
    The classes are still empty and needs to be worked on.
    
    There's still some work to be done for the packages and make everything easier to be read.

---
+++ src/it/ringmaster/nlth/model/Card.java
@@ -0,0 +1,6 @@
+package it.ringmaster.nlth.model;
+
+//Maybe the enums should be inserted here
+public class Card {
+
+}
---
+++ src/it/ringmaster/nlth/model/Status.java
@@ -0,0 +1,4 @@
+package it.ringmaster.nlth.model;
+
+public class Status {
+}
